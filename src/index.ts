fb55b0ac1deb11e63dfec95a370b9a061b1a4ab7
amalshehu <amalshehu@gmail.com>
2021-01-05 23:58:36 +0530

    Implement reduce

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
-export const reduce = (arr: number[], fn:any,acc:any): any => {
- if(arr.length === 0) return acc
- acc += fn(arr.shift())
-  return reduce(arr, fn,acc)
-};
+export const rObj = <T>(obj: Record<string, T>,fn: (acc: T, arg: T) => T,acc: T): T => {
+  const k = Object.entries(obj);
+  return k.length === 0? acc: ((acc = fn(acc, k.shift()?.[1]!)), rObj(Object.fromEntries(k), fn, acc))
+}
